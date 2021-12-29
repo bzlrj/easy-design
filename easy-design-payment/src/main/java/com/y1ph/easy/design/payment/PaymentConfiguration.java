a264cf98b80004b0ab7697f8e4a87b12be5c22e7
wft <2055305009@qq.com>
2021-12-30 05:41:20 +0800

    支付配置

---
+++ easy-design-payment/src/main/java/com/y1ph/easy/design/payment/PaymentConfiguration.java
@@ -0,0 +1,15 @@
+package com.y1ph.easy.design.payment;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 支付配置类
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+@Configuration
+public class PaymentConfiguration {
+
+
+}
