67b872f2e54e423d713a93a0547d8fe488383683
wft <2055305009@qq.com>
2021-12-30 05:25:16 +0800

    支付接口

---
+++ easy-design-payment/src/main/java/com/y1ph/easy/design/payment/service/PaymentService.java
@@ -0,0 +1,23 @@
+package com.y1ph.easy.design.payment.service;
+
+import com.y1ph.easy.design.common.BaseBean;
+import com.y1ph.easy.design.payment.beans.OrderParam;
+
+/**
+ * 支付接口
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+public interface PaymentService extends BaseBean<String> {
+
+    /**
+     * 调起第三方支付
+     *
+     * @param param    {@link OrderParam} 订单参数
+     * @param clientId {@link String} 客户端编号
+     * @param <Param>  {@link Object} 不同方式的支付返回值不同,因此这里返回一个Object
+     */
+    <Param extends OrderParam> Object payment(Param param, String clientId);
+
+}
