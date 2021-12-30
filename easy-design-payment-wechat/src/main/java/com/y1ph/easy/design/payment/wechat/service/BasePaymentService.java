256616f303c070fba10d2949d789f70e10009a60
wft <2055305009@qq.com>
2021-12-31 00:47:37 +0800

    微信支付接口抽象类

---
+++ easy-design-payment-wechat/src/main/java/com/y1ph/easy/design/payment/wechat/service/BasePaymentService.java
@@ -0,0 +1,34 @@
+package com.y1ph.easy.design.payment.wechat.service;
+
+import com.y1ph.easy.design.payment.beans.PaymentOrder;
+import com.y1ph.easy.design.payment.service.PaymentService;
+import com.y1ph.easy.design.payment.wechat.beans.WechatProperties;
+import lombok.SneakyThrows;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 微信支付接口抽象类
+ *
+ * @author WFT
+ * @since 2021/12/31
+ */
+public abstract class BasePaymentService implements PaymentService<WechatProperties<?>>, ApplicationContextAware {
+
+    private WechatPropertiesService<?> propertiesService;
+
+    @Override
+    @SneakyThrows
+    public <Param extends PaymentOrder> Object payment(Param param, String clientId) {
+        return this.payment(param,this.propertiesService.getProperties(clientId));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.propertiesService = context.getBean(WechatPropertiesService.class);
+        if (null == this.propertiesService) {
+            throw new RuntimeException(String.format("请实现%s接口", WechatPropertiesService.class.toString()));
+        }
+    }
+}
