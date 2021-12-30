69382899da76ca4e4e572e5dd422fab9a2094a2f
wft <2055305009@qq.com>
2021-12-30 15:01:34 +0800

    自定义Function接口

---
+++ easy-design-common/src/main/java/com/y1ph/easy/design/common/util/Function.java
@@ -0,0 +1,21 @@
+package com.y1ph.easy.design.common.util;
+
+/**
+ * Represents a function that accepts one argument and produces a result.
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+@FunctionalInterface
+public interface Function<T, R> {
+
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param t the function argument
+     * @return the function result
+     * @throws Exception 异常
+     */
+    R apply(T t) throws Exception;
+
+}
