3b1d7bc072daf21d7a54c8e8ed52e251807b1659
wft <2055305009@qq.com>
2021-12-30 13:28:09 +0800

    支付宝配置类

---
+++ easy-design-payment-alibaba/src/main/java/com/y1ph/easy/design/payment/alibaba/PaymentConfiguration.java
@@ -0,0 +1,13 @@
+package com.y1ph.easy.design.payment.alibaba;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 支付配置类
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+@Configuration
+public class PaymentConfiguration {
+}
