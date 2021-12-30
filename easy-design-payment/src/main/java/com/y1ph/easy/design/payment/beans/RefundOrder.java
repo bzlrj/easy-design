1e67c223d964f1b268ad083575f14dc183673d72
wft <2055305009@qq.com>
2021-12-30 23:41:15 +0800

    退款订单接口

---
+++ easy-design-payment/src/main/java/com/y1ph/easy/design/payment/beans/RefundOrder.java
@@ -0,0 +1,27 @@
+package com.y1ph.easy.design.payment.beans;
+
+import com.y1ph.easy.design.common.BaseEntity;
+
+/**
+ * 退款订单接口
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+public interface RefundOrder extends BaseEntity<Long> {
+
+    /**
+     * 获取支付订单的编号
+     *
+     * @return {@link Long}
+     */
+    Long getOrderId();
+
+    /**
+     * 获取退款金额
+     *
+     * @return {@link Integer}
+     */
+    Integer getPrice();
+
+}
