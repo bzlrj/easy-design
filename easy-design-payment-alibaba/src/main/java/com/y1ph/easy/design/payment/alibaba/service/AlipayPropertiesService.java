e152513f1256b750fe12715283a4aa0a92fb2013
wft <2055305009@qq.com>
2021-12-30 17:16:51 +0800

    支付宝配置接口

---
+++ easy-design-payment-alibaba/src/main/java/com/y1ph/easy/design/payment/alibaba/service/AlipayPropertiesService.java
@@ -0,0 +1,15 @@
+package com.y1ph.easy.design.payment.alibaba.service;
+
+import com.y1ph.easy.design.payment.alibaba.beans.AlipayProperties;
+import com.y1ph.easy.design.payment.service.PaymentPropertiesService;
+
+/**
+ * 支付宝配置接口
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+@SuppressWarnings({"SpellCheckingInspection", "WeakerAccess"})
+public interface AlipayPropertiesService<Properties extends AlipayProperties<?>> extends PaymentPropertiesService<Properties> {
+
+}
