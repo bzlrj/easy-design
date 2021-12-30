a314aa2853a660d66fce982dced756c7b8021ad1
wft <2055305009@qq.com>
2021-12-30 23:59:39 +0800

    微信支付自动配置类

---
+++ easy-design-payment-wechat/src/main/java/com/y1ph/easy/design/payment/wechat/WechatPaymentAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.y1ph.easy.design.payment.wechat;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 微信支付自动配置类
+ *
+ * @author WFT
+ * @since 2021/12/30
+ */
+@Configuration
+@ComponentScan
+@SuppressWarnings("SpellCheckingInspection")
+public class WechatPaymentAutoConfiguration {
+
+
+}
