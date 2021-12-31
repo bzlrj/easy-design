d336118f48ccf435f1e8fbccbb1dc9cba8910062
wft <2055305009@qq.com>
2022-01-01 06:52:46 +0800

    业务异常

---
+++ easy-design-website/src/main/java/com/y1ph/easy/design/website/exception/BizException.java
@@ -0,0 +1,34 @@
+package com.y1ph.easy.design.website.exception;
+
+import lombok.Getter;
+
+/**
+ * 业务异常
+ *
+ * @author WFT
+ * @since 2022/1/1
+ */
+@Getter
+public class BizException extends Exception {
+
+    /**
+     * Http状态码
+     */
+    private final int code;
+
+    /**
+     * 数据
+     */
+    private final Object data;
+
+    public BizException(String message, int code) {
+        this(message, code, null);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public BizException(String message, int code, Object data) {
+        super(message);
+        this.code = code;
+        this.data = data;
+    }
+}
