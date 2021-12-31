aadca794ab1c40a9d854f5beeb8ef41e0c5cf892
wft <2055305009@qq.com>
2022-01-01 01:11:19 +0800

    WebMvc 配置类

--- easy-design-website/src/main/java/com/y1ph/easy/design/website/WebMvcConfiguration.java
+++ easy-design-website/src/main/java/com/y1ph/easy/design/website/WebMvcConfiguration.java
@@ -1,30 +1,32 @@
 package com.y1ph.easy.design.website;
 
+import com.y1ph.easy.design.website.context.BaseHandlerInterceptor;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Collection;
+
 /**
- * Web Mvc 配置类
+ * WebMvc 配置类
  *
  * @author WFT
  * @since 2021/12/31
  */
 @Configuration
-public class WebMvcConfiguration implements ApplicationContextAware {
+public class WebMvcConfiguration implements WebMvcConfigurer, ApplicationContextAware {
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
-        return new WebMvcConfigurer(){
-            @Override
-            public void addInterceptors(InterceptorRegistry registry) {
+    /**
+     * 拦截器列表
+     */
+    private Collection<BaseHandlerInterceptor> interceptors = null;
 
-            }
-        };
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        interceptors.forEach(interceptor -> registry.addInterceptor(interceptor).addPathPatterns(interceptor.getPathPatterns()));
     }
 
 
