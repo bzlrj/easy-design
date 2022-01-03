66a2ecdc9accf159cef81e80d2efaf1d5e8f2fed
wft <2055305009@qq.com>
2022-01-03 22:20:33 +0800

    添加配置类

---
+++ easy-design-data-redis/src/main/java/com/y1ph/easy/design/data/redis/RedisAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.y1ph.easy.design.data.redis;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis配置类
+ *
+ * @author WFT
+ * @since 2022/1/3
+ */
+@Configuration
+@ComponentScan
+public class RedisAutoConfiguration {
+
+
+}
