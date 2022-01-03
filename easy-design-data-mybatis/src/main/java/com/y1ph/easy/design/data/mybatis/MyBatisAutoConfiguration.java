82db9d5e4f59392006568e5a4dc9032fa2994d45
wft <2055305009@qq.com>
2022-01-04 02:11:38 +0800

    添加配置文件

---
+++ easy-design-data-mybatis/src/main/java/com/y1ph/easy/design/data/mybatis/MyBatisAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.y1ph.easy.design.data.mybatis;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis-Plus自动化配置
+ *
+ * @author WFT
+ * @since 2022/1/4
+ */
+@Configuration
+@ComponentScan
+public class MyBatisAutoConfiguration {
+
+
+}
