02321d869b6fa065d564983c082f5f26620c314c
wft <2055305009@qq.com>
2022-01-04 02:00:27 +0800

    添加配置

--- easy-design-data-redis/src/main/java/com/y1ph/easy/design/data/redis/RedisAutoConfiguration.java
+++ easy-design-data-redis/src/main/java/com/y1ph/easy/design/data/redis/RedisAutoConfiguration.java
@@ -1,7 +1,10 @@
 package com.y1ph.easy.design.data.redis;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * Redis配置类
@@ -13,5 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class RedisAutoConfiguration {
 
-
+    @Bean
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
 }
