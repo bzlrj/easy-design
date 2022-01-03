8da99170c44179150a4268f9ed47ccdd0bfe512c
wft <2055305009@qq.com>
2022-01-03 23:38:45 +0800

    公共的Redis接口

--- easy-design-data-redis/src/main/java/com/y1ph/easy/design/data/redis/service/BaseRedisService.java
+++ easy-design-data-redis/src/main/java/com/y1ph/easy/design/data/redis/service/BaseRedisService.java
@@ -1,7 +1,5 @@
 package com.y1ph.easy.design.data.redis.service;
 
-import org.springframework.data.redis.core.RedisTemplate;
-
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -12,31 +10,21 @@ import java.util.concurrent.TimeUnit;
  * @author WFT
  * @since 2022/1/3
  */
-public abstract class BaseRedisService {
-
-    /**
-     * (non-Javadoc)
-     * @return {@link RedisTemplate}
-     */
-    protected abstract RedisTemplate<String, Object> getTemplate();
+public interface BaseRedisService {
 
     /**
      * 删除缓存
      *
      * @param key {@link String}
      */
-    public void delete(String key) {
-        this.getTemplate().delete(key);
-    }
+    void delete(String key);
 
     /**
      * 删除缓存
      *
      * @param keys {@link Collection}
      */
-    public void delete(Collection<String> keys) {
-        this.getTemplate().delete(keys);
-    }
+    void delete(Collection<String> keys);
 
     /**
      * 查询符合条件的所有密钥
@@ -44,9 +32,7 @@ public abstract class BaseRedisService {
      * @param pattern {@link String}
      * @return {@link Set}
      */
-    public Set<String> keys(String pattern) {
-        return this.getTemplate().keys(pattern);
-    }
+    Set<String> keys(String pattern);
 
     /**
      * 获取缓存的有效期
@@ -54,9 +40,7 @@ public abstract class BaseRedisService {
      * @param key {@link String} 缓存名称
      * @return {@link Long} 有效期（单位：秒）
      */
-    public Long expire(String key) {
-        return this.expire(key, TimeUnit.SECONDS);
-    }
+    Long expire(String key);
 
     /**
      * 获取缓存的有效期
@@ -65,10 +49,7 @@ public abstract class BaseRedisService {
      * @param unit {@link TimeUnit} 时间单位
      * @return {@link Long}
      */
-    @SuppressWarnings("WeakerAccess")
-    public Long expire(String key, TimeUnit unit) {
-        return this.getTemplate().getExpire(key, unit);
-    }
+    Long expire(String key, TimeUnit unit);
 
     /**
      * 设置缓存的有效期
@@ -76,9 +57,7 @@ public abstract class BaseRedisService {
      * @param key     {@link String} 缓存名称
      * @param timeout {@link Long} 有效期（单位：秒）
      */
-    public void expire(String key, long timeout) {
-        this.expire(key, timeout, TimeUnit.SECONDS);
-    }
+    void expire(String key, long timeout);
 
     /**
      * 设置缓存的有效期
@@ -87,9 +66,6 @@ public abstract class BaseRedisService {
      * @param timeout {@link Long} 有效期
      * @param unit    {@link TimeUnit} 时间单位
      */
-    @SuppressWarnings("WeakerAccess")
-    public void expire(String key, long timeout, TimeUnit unit) {
-        this.getTemplate().expire(key, timeout, unit);
-    }
+    void expire(String key, long timeout, TimeUnit unit);
 
 }
