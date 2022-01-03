a3161faf0e437283c73201920ed094e980a0d99b
wft <2055305009@qq.com>
2022-01-04 02:39:24 +0800

    定义查询接口

---
+++ easy-design-data-mybatis/src/main/java/com/y1ph/easy/design/data/mybatis/service/BaseService.java
@@ -0,0 +1,105 @@
+package com.y1ph.easy.design.data.mybatis.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.y1ph.easy.design.common.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * 公共服务层接口
+ *
+ * @author WFT
+ * @since 2022/1/4
+ */
+public interface BaseService<Entity extends BaseEntity<?>> {
+
+    /**
+     * 根据编号查询
+     *
+     * @param id {@link Serializable} 编号
+     * @return {@link BaseEntity} 查询结果
+     */
+    Entity select(Serializable id);
+
+    /**
+     * 根据查询条件查询
+     *
+     * @param consumer {@link Consumer} 查询条件
+     * @return {@link BaseEntity} 如果结果不唯一,将抛出异常
+     */
+    Entity select(Consumer<LambdaQueryWrapper<Entity>> consumer);
+
+    /**
+     * 查询列表
+     *
+     * @return {@link List} 查询结果
+     */
+    default List<Entity> query() {
+        return this.query(wrapper -> {
+
+        });
+    }
+
+    /**
+     * 根据编号列表查询
+     *
+     * @param list {@link List} 编号列表不能为空
+     * @return {@link List} 查询结果
+     */
+    default List<Entity> query(Collection<? extends Serializable> list) {
+        return this.query(wrapper -> wrapper.in(Entity::getId, list));
+    }
+
+    /**
+     * 根据条件查询列表
+     *
+     * @param consumer {@link Consumer} 查询条件
+     * @return {@link List} 查询结果
+     */
+    List<Entity> query(Consumer<LambdaQueryWrapper<Entity>> consumer);
+
+    /**
+     * 查询总记录数
+     *
+     * @return {@link Integer}
+     */
+    default int count() {
+        return this.count(wrapper -> {
+
+        });
+    }
+
+    /**
+     * 根据条件查询总记录数
+     *
+     * @param consumer {@link Consumer} 查询条件
+     * @return {@link Integer} 总记录数
+     */
+    int count(Consumer<LambdaQueryWrapper<Entity>> consumer);
+
+    /**
+     * 分页查询
+     *
+     * @param page {@link IPage} 分页条件
+     * @return {@link IPage} 查询结果
+     */
+    default IPage<Entity> page(IPage<Entity> page) {
+        return this.page(page, wrapper -> {
+
+        });
+    }
+
+    /**
+     * 根据条件分页查询
+     *
+     * @param page     {@link IPage} 分页条件
+     * @param consumer {@link Consumer} 查询条件
+     * @return {@link IPage} 查询结果
+     */
+    IPage<Entity> page(IPage<Entity> page, Consumer<LambdaQueryWrapper<Entity>> consumer);
+
+}
