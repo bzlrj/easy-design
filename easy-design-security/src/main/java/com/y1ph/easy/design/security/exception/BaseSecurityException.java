3dc5bc72e46e1b17e741e02fc6d010afb041f655
wft <2055305009@qq.com>
2022-01-04 13:55:30 +0800

    权限异常父类

---
+++ easy-design-security/src/main/java/com/y1ph/easy/design/security/exception/BaseSecurityException.java
@@ -0,0 +1,17 @@
+package com.y1ph.easy.design.security.exception;
+
+import com.y1ph.easy.design.website.exception.BizException;
+
+/**
+ * 权限异常父类
+ *
+ * @author WFT
+ * @since 2022/1/4
+ */
+public class BaseSecurityException extends BizException {
+
+    public BaseSecurityException(String message, int code) {
+        super(message, code);
+    }
+
+}
