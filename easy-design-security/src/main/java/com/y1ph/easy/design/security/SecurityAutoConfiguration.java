b696ba3d4bf31debde3336a0898bb28f0ad8f233
wft <2055305009@qq.com>
2022-01-04 13:44:01 +0800

    权限配置

---
+++ easy-design-security/src/main/java/com/y1ph/easy/design/security/SecurityAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.y1ph.easy.design.security;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 权限自动化配置
+ *
+ * @author WFT
+ * @since 2022/1/4
+ */
+@ComponentScan
+@Configuration
+public class SecurityAutoConfiguration {
+
+
+
+}
