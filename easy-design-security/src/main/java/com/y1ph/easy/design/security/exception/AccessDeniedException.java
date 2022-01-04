d843caae61c895c17bd895004ceb2cc1244fa9cf
wft <2055305009@qq.com>
2022-01-04 13:55:38 +0800

    403 异常

---
+++ easy-design-security/src/main/java/com/y1ph/easy/design/security/exception/AccessDeniedException.java
@@ -0,0 +1,22 @@
+package com.y1ph.easy.design.security.exception;
+
+import com.y1ph.easy.design.website.constant.HttpStatus;
+
+/**
+ * 403 异常
+ *
+ * @author WFT
+ * @since 2022/1/4
+ */
+public class AccessDeniedException extends BaseSecurityException {
+
+    public AccessDeniedException(){
+        this("You don't have permission to access on this server.");
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public AccessDeniedException(String message) {
+        super(message, HttpStatus.FORBIDDEN);
+    }
+
+}
