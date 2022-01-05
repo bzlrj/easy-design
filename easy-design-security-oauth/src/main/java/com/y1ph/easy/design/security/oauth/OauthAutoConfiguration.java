610c3144dd84a8371c182b237bfc5e89311fe896
wft <2055305009@qq.com>
2022-01-05 14:26:50 +0800

    Oauth 配置类

---
+++ easy-design-security-oauth/src/main/java/com/y1ph/easy/design/security/oauth/OauthAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.y1ph.easy.design.security.oauth;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Oauth 配置类
+ *
+ * @author WFT
+ * @since 2022/1/5
+ */
+@Configuration
+@ComponentScan
+public class OauthAutoConfiguration {
+
+
+}
