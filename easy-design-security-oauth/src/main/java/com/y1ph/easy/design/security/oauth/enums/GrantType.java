e03a93775cc64b1a5726fc553ce31255eb90f112
wft <2055305009@qq.com>
2022-01-05 17:43:59 +0800

    认证模式枚举

---
+++ easy-design-security-oauth/src/main/java/com/y1ph/easy/design/security/oauth/enums/GrantType.java
@@ -0,0 +1,30 @@
+package com.y1ph.easy.design.security.oauth.enums;
+
+import com.y1ph.easy.design.common.BaseEnum;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 认证模式
+ *
+ * @author WFT
+ * @since 2022/1/5
+ */
+@Getter
+@RequiredArgsConstructor
+public enum GrantType implements BaseEnum<Integer> {
+
+    /**
+     * 密码模式
+     */
+    password(1),
+
+    /**
+     * 授权码模式
+     */
+    authorization_code(2),
+    ;
+
+    private final int value;
+
+}
